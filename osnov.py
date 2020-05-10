e625ce8f547ef51a76f526e32d8efaf37961ae86
sasha-sugako <aleksandrasugako@yandex.ru>
2020-05-10 12:36:34 +0300

    Попытка 1

--- osnov.py
+++ osnov.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect
 import sqlite3
 import random
 from data import db_session
+from requests import request
 con = sqlite3.connect("game.db")
 cur = con.cursor()
 app = Flask(__name__)
@@ -13,31 +14,31 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def login():
     params = {
         'vopr': ['Фильмы', 'Книги', 'Фауна', 'Еда', 'Солянка'],
-        'list': [10, 20, 30, 40, 50]
+        'list': [10, 20, 30, 40, 50],
+        'id_vopr': [1, 2, 3, 4, 5]
     }
     return render_template('secur.html', **params)
 
-
+    
 @app.route('/success')
 def success():
-    params = {}
+    params = {'form': {
+        'vopr': obr_vop(num_vopr)
+    }}
     return render_template('seccess.html', **params)
 
 
 def obr_vop(num_vopr):
-    result = cur.execute("""SELECT vopr FROM second_page WHERE num_v =""" + str(num_vopr)).fetchall()
-    result2 = cur.execute("""SELECT kolvo_isp FROM second_page WHERE num_v =""" + str(num_vopr)).fetchall()
-    a = random.randint(0, 4)
-    while result2[a][0] != 0:
-        a = random.randint(0, 4)
-    otv = result[a][0]
-    return otv
+    result = []
+    for i in session.query(Second_page).filter(Second_page.kol_isp == 0, Second_page.vopr_id == num_vopr):
+        result.append(i)
+    a = random.randint(0, len(result))
+    vopr_ud = session.query(Second_page).filter(Second_page.title == result[a][0])
+    vopr_ud.kol_isp = 1
+    session.commit()
+    return result[a][0]
 
 
-def normal_form(text):
-    return text
-
-print(obr_vop(1))
 if __name__ == '__main__':
     db_session.global_init("db/blogs.sqlite")
     app.run(port=8080, host='127.0.0.1')
